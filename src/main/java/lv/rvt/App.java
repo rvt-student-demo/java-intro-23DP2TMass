e551ff902ca87976991266fbd71a8843c3b76eca
23DP2TMass <vikitri333@gmail.com>
2024-11-14 09:08:25 +0000

    Various Variables

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,14 +1,18 @@
 package lv.rvt;
 
-
+public class App {
+public static void main(String[] args) {
     String text = "Zetor";
     int wholeNumber = 9000;
     double floatingPoint = 0.1;
     
     System.out.println("Chicken: " + wholeNumber);
     System.out.println("Bacon (kg): " + floatingPoint);
-    System.out.println(Tractor: " + text);
-    System.out.println("And finally, a summary: " + wholeNumber) ; 
+    System.out.println("Tractor: " + text);
+    System.out.println("And finally, a summary: " ) ; 
+    System.out.println( wholeNumber ) ; 
     System.out.println( floatingPoint ) ; 
     System.out.println( text ) ; 
 }
+}
+
