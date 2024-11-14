5294aa22f6cf83a6cc31cbab581b500f14f4a42f
23DP2TMass <vikitri333@gmail.com>
2024-11-14 09:13:04 +0000

    Double Input

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class App {
 public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Give a number: ");
-        int value = Integer.valueOf(scanner.nextLine());
-        System.out.println("You gave the number: " + value);
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Give a number:");
+    double value = Double.valueOf(scanner.nextLine());
+    System.out.println("You gave the number 123.5" + value);
     }
 }
 
