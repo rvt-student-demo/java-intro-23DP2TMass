6f1ad98f325a0080846c9bb8a7cd2cbfd5938a49
23DP2TMass <vikitri333@gmail.com>
2024-11-14 09:10:51 +0000

    Integer Input

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,18 +1,15 @@
 package lv.rvt;
 
+import java.util.Scanner;
+
 public class App {
 public static void main(String[] args) {
-    String text = "Zetor";
-    int wholeNumber = 9000;
-    double floatingPoint = 0.1;
-    
-    System.out.println("Chicken: " + wholeNumber);
-    System.out.println("Bacon (kg): " + floatingPoint);
-    System.out.println("Tractor: " + text);
-    System.out.println("And finally, a summary: " ) ; 
-    System.out.println( wholeNumber ) ; 
-    System.out.println( floatingPoint ) ; 
-    System.out.println( text ) ; 
+            Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give a number: ");
+        int value = Integer.valueOf(scanner.nextLine());
+        System.out.println("You gave the number: " + value);
+    }
 }
-}
+
 
